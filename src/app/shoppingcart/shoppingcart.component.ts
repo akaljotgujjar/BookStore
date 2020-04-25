2865f3d69e846f9176f8a7e4e77daa6536041edd
saiaungkoko <saiaungkoko@mail.fresnostate.edu>
2020-04-24 19:15:26 -0700

    add shopping cast

--- src/app/shoppingcart/shoppingcart.component.ts
+++ src/app/shoppingcart/shoppingcart.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpService } from '../../shared-service/http.service';
+import { ToastService } from '../toast/toast.service';
+
+interface IHome {
+  id?: number;
+  bookImage: string;
+  bookName: string;
+  bookAuthor: string;
+  bookType: string;
+  bookPrice: string;
+  bookQuantity: string;
+}
 
 @Component({
   selector: 'app-shoppingcart',
@@ -7,13 +19,35 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./shoppingcart.component.css']
 })
 export class ShoppingcartComponent implements OnInit {
-  data: any = {};
-  constructor(private activatedRoute: ActivatedRoute) { }
 
-  ngOnInit() {
-    this.activatedRoute.params.subscribe((sss) => {
-      this.data = sss;
-    });
+  shoppingcarts = [];
+  homeCard: Array<IHome> = [];
+  loggedIn = false;
+  bookParams = '';
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private http: HttpService,
+    private router: Router,
+    private toastService: ToastService) { }
+
+  async ngOnInit() {
+    const token = localStorage.getItem('id_token');
+    if (token == null) {
+      this.loggedIn = false;
+      this.router.navigate(['signIn']);
+    } else {
+      this.loggedIn = true;
+    }
+    await this.refresh();
+  }
+
+  async refresh() {
+    this.shoppingcarts = await this.getshoppingcart('shoppingcart');
+  }
+
+  async getshoppingcart(path: string) {
+    const resp = await this.http.get(path);
+    return resp;
   }
 
 }
